e0131901c7c560d6ddcbac996735e109995097a8
puh <->
2020-04-22 02:15:30 +0300

    Move cell creation task to model

--- app/src/main/java/com/puuuuh/ingressmap/map/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/puuuuh/ingressmap/map/viewmodel/MapViewModel.kt
@@ -7,20 +7,27 @@ import com.puuuuh.ingressmap.map.model.Entity
 import com.puuuuh.ingressmap.map.model.MapModel
 import com.puuuuh.ingressmap.map.model.OnDataReadyCallback
 import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.PolylineOptions
+import com.google.common.geometry.S2CellId
+import com.puuuuh.ingressmap.map.model.OnCellsReadyCallback
 
-class MapViewModel(userData: UserData) : ViewModel(), OnDataReadyCallback {
+class MapViewModel(userData: UserData) : ViewModel(), OnDataReadyCallback, OnCellsReadyCallback {
     private val _user = MutableLiveData<UserData>()
-    val user: LiveData<UserData> = _user
+    private val user: LiveData<UserData> = _user
     private val model: MapModel = MapModel()
 
     private val _portals = MutableLiveData<Map<String, Entity>>()
     val portals: LiveData<Map<String, Entity>> = _portals
+
+    private val _cellLines = MutableLiveData<Map<S2CellId, PolylineOptions>>()
+    val cellLines: LiveData<Map<S2CellId, PolylineOptions>> = _cellLines
 
     init {
         _user.value = userData
     }
 
     fun updateRegion(r: LatLngBounds, zoom: Int) {
+        model.updateCells(zoom, r, this)
         model.refreshData(user.value!!, r, zoom, this)
     }
 
@@ -35,5 +42,18 @@ class MapViewModel(userData: UserData) : ViewModel(), OnDataReadyCallback {
             new[i.guid] = i
         }
         _portals.postValue(new)
+    }
+
+    override fun onCellsReady(data: Map<S2CellId, PolylineOptions>) {
+        synchronized(_cellLines) {
+            val new = mutableMapOf<S2CellId, PolylineOptions>()
+            for (i in data) {
+                if (new.containsKey(i.key).and(new.containsValue(i.value))) {
+                    continue
+                }
+                new[i.key] = i.value
+            }
+            _cellLines.postValue(new)
+        }
     }
 }
