27f299d7ba929b82ebb5fa6e3e05779bdb5c0aa7
puh <->
2020-04-23 21:13:27 +0300

    A ton of changes Update version string, change markers, add options for disable fields, portals, links, change colors, move some logic to viewmodel

--- app/src/main/java/com/puuuuh/ingressmap/map/view/MapActivity.kt
+++ app/src/main/java/com/puuuuh/ingressmap/map/view/MapActivity.kt
@@ -1,51 +1,111 @@
 package com.puuuuh.ingressmap.map.view
 
 import android.graphics.Color
-import android.os.AsyncTask
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import com.puuuuh.ingressmap.R
-import com.puuuuh.ingressmap.map.viewmodel.MapViewModel
-import com.puuuuh.ingressmap.map.viewmodel.UserData
 import com.google.android.gms.common.api.Status
-import com.google.android.gms.maps.*
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import com.google.android.libraries.places.api.model.Place
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
-import com.google.common.geometry.*
+import com.google.common.geometry.S2CellId
+import com.puuuuh.ingressmap.R
+import com.puuuuh.ingressmap.map.viewmodel.MapViewModel
+import com.puuuuh.ingressmap.map.viewmodel.UserData
 import kotlinx.android.synthetic.main.activity_map.*
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnCameraIdleListener {
     private lateinit var mMap: GoogleMap
     private var lines = hashMapOf<S2CellId, Polyline>()
-    private val portals = hashMapOf<String, Marker>()
+    private var portals = mutableMapOf<String, Marker>()
+    private var fields = mutableMapOf<String, Polygon>()
+    private var links = mutableMapOf<String, Polyline>()
     private lateinit var mapViewModel: MapViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_map)
+        setSupportActionBar(toolbar)
+
         val user = UserData(token=intent.getStringExtra("token"), csrfToken=intent.getStringExtra("csrfToken"))
         mapViewModel = MapViewModel(user)
         mapViewModel.portals.observe(this, androidx.lifecycle.Observer {
+            val newPortals = mutableMapOf<String, Marker>()
             for (i in it) {
-                if (!portals.containsKey(i.key)) {
-                    val resource = if (i.value.team == "E") {R.drawable.ic_green_portal} else {R.drawable.ic_blue_portal}
+                val old = portals.remove(i.key)
+                if (old == null) {
+                    val resource = if (i.value.team == "E") {
+                        R.drawable.ic_green_portal
+                    } else if (i.value.team == "R") {
+                        R.drawable.ic_blue_portal
+                    } else {
+                        R.drawable.ic_white_portal
+                    }
                     val point = LatLng(i.value.lat, i.value.lng)
                     val m = MarkerOptions()
                         .position(point)
                         .title(i.value.name)
                         .icon(BitmapDescriptorFactory.fromResource(resource))
                         .anchor(0.5f, 0.5f)
-                        .visible(false)
-                    portals[i.key] = mMap.addMarker(m)
+                    newPortals[i.key] = mMap.addMarker(m)
+                } else {
+                    newPortals[i.key] = old
                 }
             }
-            val viewport = mMap.projection.visibleRegion.latLngBounds
-            val portalsEnabled = mMap.cameraPosition.zoom > 15
-            for (p in portals) {
-                p.value.isVisible = portalsEnabled && viewport.contains(p.value.position)
+            for (l in portals) {
+                l.value.remove()
             }
+            portals = newPortals
+        })
+        mapViewModel.links.observe(this, androidx.lifecycle.Observer { data ->
+            val newLinks = mutableMapOf<String, Polyline>()
+            for (i in data) {
+                val old = links.remove(i.key)
+                if (old == null) {
+                    val color = if (i.value.team == "E") {Color.GREEN} else {Color.BLUE}
+                    val m = PolylineOptions()
+                        .add(i.value.points[0].LatLng, i.value.points[1].LatLng)
+                        .width(2f)
+                        .color(color)
+
+                    newLinks[i.key] = mMap.addPolyline(m)
+                } else {
+                    newLinks[i.key] = old
+                }
+            }
+            for (l in links) {
+                l.value.remove()
+            }
+            links = newLinks
+        })
+        mapViewModel.fields.observe(this, androidx.lifecycle.Observer { data ->
+            val newFields = mutableMapOf<String, Polygon>()
+            for (i in data) {
+                val old = fields.remove(i.key)
+                if (old == null) {
+                    val color = if (i.value.team == "E") {Color.argb(100, 0, 255, 0)} else {Color.argb(100, 0, 0, 255)}
+                    val strokeColor = if (i.value.team == "E") {Color.argb(255, 0, 255, 0)} else {Color.argb(255, 0, 140, 255)}
+                    val m = PolygonOptions()
+                        .add(i.value.points[0].LatLng, i.value.points[1].LatLng, i.value.points[2].LatLng)
+                        .fillColor(color)
+                        .strokeWidth(4f)
+                        .strokeColor(strokeColor)
+
+                    newFields[i.key] = mMap.addPolygon(m)
+                } else {
+                    newFields[i.key] = old
+                }
+            }
+            for (l in fields) {
+                l.value.remove()
+            }
+            fields = newFields
         })
         mapViewModel.cellLines.observe(this, androidx.lifecycle.Observer { data ->
             val newLines = hashMapOf<S2CellId, Polyline>()
@@ -82,13 +142,41 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnCamera
         autocomplete.setPlaceFields(listOf(Place.Field.LAT_LNG))
         autocomplete.setOnPlaceSelectedListener(object : PlaceSelectionListener {
             override fun onPlaceSelected(place: Place) {
-                mMap.animateCamera(CameraUpdateFactory
-                    .zoomTo(13f))
-                mMap.animateCamera(CameraUpdateFactory
-                    .newLatLng(place.latLng))
+                val pos = CameraPosition.Builder()
+                    .zoom(15f)
+                    .target(place.latLng)
+                    .build()
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(pos))
             }
             override fun onError(status: Status) {}
         })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when (item?.itemId) {
+            R.id.showPortals -> {
+                item.isChecked = !item.isChecked
+                mapViewModel.setPortalsVisible(item.isChecked)
+                return true
+            }
+            R.id.showFields -> {
+                item.isChecked = !item.isChecked
+                mapViewModel.setFieldsVisible(item.isChecked)
+                return true
+            }
+            R.id.showLinks -> {
+                item.isChecked = !item.isChecked
+                mapViewModel.setLinksVisible(item.isChecked)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
@@ -102,10 +190,6 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback,  GoogleMap.OnCamera
 
     override fun onCameraIdle() {
         mapViewModel.updateRegion(mMap.projection.visibleRegion.latLngBounds, mMap.cameraPosition.zoom.toInt())
-        val portalsEnabled = mMap.cameraPosition.zoom > 15
-        val viewport = mMap.projection.visibleRegion.latLngBounds
-        for (p in portals) {
-            p.value.isVisible = portalsEnabled && viewport.contains(p.value.position)
-        }
     }
 }
+
--- app/src/main/java/com/puuuuh/ingressmap/map/viewmodel/MapViewModel.kt
+++ app/src/main/java/com/puuuuh/ingressmap/map/viewmodel/MapViewModel.kt
@@ -3,18 +3,34 @@ package com.puuuuh.ingressmap.map.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.puuuuh.ingressmap.map.model.Entity
-import com.puuuuh.ingressmap.map.model.MapModel
-import com.puuuuh.ingressmap.map.model.OnDataReadyCallback
+import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.common.geometry.S2CellId
-import com.puuuuh.ingressmap.map.model.OnCellsReadyCallback
+import com.puuuuh.ingressmap.map.model.*
+
+data class CellData(val portals: Map<String, Entity>, val links: Map<String, Link>, val fields: Map<String, Field>) {
+
+}
 
 class MapViewModel(userData: UserData) : ViewModel(), OnDataReadyCallback, OnCellsReadyCallback {
     private val _user = MutableLiveData<UserData>()
     private val user: LiveData<UserData> = _user
     private val model: MapModel = MapModel()
+
+    // All cached entities
+    private val allPortals = mutableMapOf<String, Entity>()
+    private val allLinks = mutableMapOf<String, Link>()
+    private val allFields = mutableMapOf<String, Field>()
+
+    // Visibility
+    private var showPortal = false
+    private var showLinks = false
+    private var showFields = false
+
+    // Entities in current viewport
+    private var viewport = LatLngBounds(LatLng(0.0, 0.0), LatLng(0.0, 0.0))
+    private var zoom = 21
 
     private val _portals = MutableLiveData<Map<String, Entity>>()
     val portals: LiveData<Map<String, Entity>> = _portals
@@ -22,26 +38,124 @@ class MapViewModel(userData: UserData) : ViewModel(), OnDataReadyCallback, OnCel
     private val _cellLines = MutableLiveData<Map<S2CellId, PolylineOptions>>()
     val cellLines: LiveData<Map<S2CellId, PolylineOptions>> = _cellLines
 
+    private val _links = MutableLiveData<Map<String, Link>>()
+    val links: LiveData<Map<String, Link>> = _links
+
+    private val _fields = MutableLiveData<Map<String, Field>>()
+    val fields: LiveData<Map<String, Field>> = _fields
+
+    private val _cellCache = mutableMapOf<String, CellData>()
+
     init {
         _user.value = userData
     }
 
-    fun updateRegion(r: LatLngBounds, zoom: Int) {
-        model.updateCells(zoom, r, this)
-        model.refreshData(user.value!!, r, zoom, this)
+    fun updateRegion(r: LatLngBounds, z: Int) {
+        viewport = r
+        zoom = z
+        model.updateCells(z, r, this)
+        model.refreshData(user.value!!, r, z, this)
+        updateVisibleLinks()
+        updateVisiblePortals()
+        updateVisibleFields()
     }
 
-    override fun onDataReady(data: List<Entity>) {
-        if (data.isEmpty()) {
-            return
-        }
-        val old = _portals.value
+    private fun updateVisibleFields() {
+        _fields.postValue(
+            if (!showFields || zoom < 13) {
+                mapOf()
+            } else {
+                allFields.filter { field -> field.value.points.any { viewport.contains(it.LatLng) } }
+            })
+    }
 
-        val new = old?.toMutableMap() ?: mutableMapOf()
-        for (i in data) {
-            new[i.guid] = i
+    private fun updateVisibleLinks() {
+        _links.postValue(
+            if (!showLinks || zoom < 14) {
+                mapOf()
+            } else {
+                allLinks.filter { link -> link.value.points.any { viewport.contains(it.LatLng) } }
+            })
+    }
+
+    private fun updateVisiblePortals() {
+        _portals.postValue(
+            if (!showPortal || zoom < 14.9) {
+                mapOf()
+            } else {
+                allPortals.filter { viewport.contains(LatLng(it.value.lat, it.value.lng)) }
+            }
+        )
+    }
+
+    fun setPortalsVisible(value: Boolean) {
+        if (showPortal != value) {
+            showPortal = value
+            updateVisiblePortals()
         }
-        _portals.postValue(new)
+    }
+    fun setFieldsVisible(value: Boolean) {
+        if (showFields != value) {
+            showFields = value
+            updateVisibleFields()
+        }
+    }
+    fun setLinksVisible(value: Boolean) {
+        if (showLinks != value) {
+            showLinks = value
+            updateVisibleLinks()
+        }
+    }
+
+    override fun onCellDataReceived(
+        cellId: String,
+        portals: Map<String, Entity>,
+        links: Map<String, Link>,
+        fields: Map<String, Field>
+    ) {
+        val cachedVersion = _cellCache[cellId]
+        if (cachedVersion != null) {
+            val removedPortals = cachedVersion.portals.keys.minus(portals.keys)
+            removedPortals.map { allPortals.remove(it) }
+            for (p in portals) {
+                val old = cachedVersion.portals[p.key]
+                if (old != p.value) {
+                    allPortals[p.key] = p.value
+                }
+            }
+
+            val removedLinks = cachedVersion.links.keys.minus(links.keys)
+            removedLinks.map { allLinks.remove(it) }
+            for (p in links) {
+                val old = cachedVersion.links[p.key]
+                if (old != p.value) {
+                    allLinks[p.key] = p.value
+                }
+            }
+
+            val removedFields = cachedVersion.fields.keys.minus(fields.keys)
+            removedFields.map { allFields.remove(it) }
+            for (p in fields) {
+                val old = cachedVersion.fields[p.key]
+                if (old != p.value) {
+                    allFields[p.key] = p.value
+                }
+            }
+        } else {
+            for (p in portals) {
+                allPortals[p.key] = p.value
+            }
+            for (p in links) {
+                allLinks[p.key] = p.value
+            }
+            for (p in fields) {
+                allFields[p.key] = p.value
+            }
+        }
+        _cellCache[cellId] = CellData(portals, links, fields)
+        updateVisibleFields()
+        updateVisibleLinks()
+        updateVisiblePortals()
     }
 
     override fun onCellsReady(data: Map<S2CellId, PolylineOptions>) {
